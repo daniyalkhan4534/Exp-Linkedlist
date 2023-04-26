492f3429313138ce5b7ef46939c2c797ebcfd3b7
daniyalkhan4534 <127819492+daniyalkhan4534@users.noreply.github.com>
2023-04-26 11:12:12 +0530

    Update Exp-Linkedlist.c

--- Exp-Linkedlist.c
+++ Exp-Linkedlist.c
@@ -1,31 +1,22 @@
-#include<stdio.h>
-#include<stdlib.h>
-void display();
-struct node
-{
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
 	int data;
-	struct node*next;
+	struct node *next;
 };
-struct node *start;
-int main()
-{
-	struct node *newnode1 = (struct node*) malloc(sizeof(struct node));
-	struct node *newnode2 = (struct node*) malloc(sizeof(struct node));
-	start = newnode1;
-	newnode1->data = 2;
-	newnode1->next = newnode2;
-	newnode2->data = 3;
-	newnode2->next = NULL;
-	display();
-	return 0; 
-}
-void display()
-{
-	struct node *temp;
-	temp = start;
-	while(temp!=NULL)
-	{
-		printf("data=%d,self addr = %d, next addr = %d\n",temp->data,temp,temp->next);
-		temp=temp->next;
-	}
+int main(){
+	struct node *newnode1,*newnode2, *newnode3;
+	newnode1=(struct node*)malloc(sizeof(struct node));
+	newnode1-> data = 20;
+	newnode2 = (struct node*)malloc(sizeof(struct node));
+	newnode2-> data = 30;
+	newnode1 -> next=newnode2;
+	
+	newnode3=(struct node*)malloc(sizeof(struct node));
+	newnode2 -> next = newnode3;
+	newnode3 -> data = 40;
+	newnode3 -> next = newnode1;
+	printf("data=%d selfaddr=%d nextaddr = %d\n", newnode1 -> data, newnode1, newnode1 ->next);
+	printf("data=%d selfaddr=%d nextaddr = %d\n", newnode2 -> data, newnode2,newnode2 ->next );
+	printf("data=%d selfaddr=%d nextaddr = %d\n", newnode3 -> data, newnode3, newnode3 -> next);
 }
